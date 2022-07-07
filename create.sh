748c3ab51563ab6ebaf7e4333299634bca712c22
vivianeflowt <vivianeflowt@gmail.com>
2022-07-07 20:06:17 -0300

    step

---
+++ create.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clear
+
+
+pnpm install @nestjs/config
+
+# pnpm install class-transformer class-validator
+# pnpm install express @nestjs/platform-express
+# pnpm install lodash bluebird ts-mixer morgan
+# pnpm install compression chalk@4
+
+# pnpm install -D @types/node @types/express @types/jest
+
+
+
+# npx typesync
+# rm -rf ./node_modules
+
+
+
--- packages/utils/ansi.ts
+++
@@ -1,9 +0,0 @@
-const ansiRegex =
-  /[\u001B\u009B][[\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\d/#&.:=?%@~_]+)*|[a-zA-Z\d]+(?:;[-a-zA-Z\d\/#&.:=?%@~_]*)*)?\u0007)|(?:(?:\d{1,4}(?:;\d{0,4})*)?[\dA-PR-TZcf-nq-uy=><~]))/g
-
-export function stringWidth(str: string): number {
-  return str.replace(ansiRegex, '').length
-}
-export function stripAnsi(str: string): string {
-  return str.replace(ansiRegex, '')
-}
